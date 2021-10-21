bb8685b3b3baf151e9cb5cd9b9dc9f2e0593b25e
Jonathan <jonathanrios77@yahoo.com>
2021-10-21 12:11:38 -0600

    All functions created for project

--- exercise_1_api/server.py
+++ exercise_1_api/server.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -19,3 +18,37 @@ def games_show(game_id):
         if game['id'] == game_id:
             return game
     return{ "error": "Not Found" }, 404
+
+@app.route('/movies', methods=['POST'])
+def games_create():
+    new_game = request.get_json()
+    new_game['id'] = str(len(games) + 1 )
+    games.append(new_game)
+    return{ 'message': 'Game created successfully' }, 201
+
+@app.route('/games/<game_id>', methods=['PATCH'])
+def games_update(game_id):
+    updates = request.get_json()
+
+    for game in games:
+        if game['id'] == game_id:
+            game.update(updates)
+            return { 'message': 'Game was updated successfully' }, 201
+
+@app.route('/games/<game_id>', methods=['DELETE'])
+def games_delete(game_id):
+    found_game_idx = None
+
+    for i in range(len(games)): # 0, 1, 2
+      if games[i]['id'] == game_id:
+        found_game_idx = i
+        break
+  
+    if found_game_idx != None:
+      games.pop(found_game_idx)
+      return { 'message': 'Game deleted successfully' }, 201
+
+    return { 'error': 'Not Found' }, 404
+
+
+app.run()
