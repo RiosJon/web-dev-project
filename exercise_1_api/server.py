d60d384498bcf1a3e5876cac542b43ea6e600883
Jonathan <jonathanrios77@yahoo.com>
2021-10-24 19:41:55 -0600

    Index template completed

--- exercise_1_api/server.py
+++ exercise_1_api/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -8,14 +8,15 @@ games = [
 {"id": '3', 'name': 'Gears of War', 'year': 2006}
 ]
 
+
 @app.route('/games')
 def games_index():
-    return jsonify(games)
+  return(render_template('/templates/index.html'))
 
 @app.route('/games/<games_id>')
-def games_show(game_id):
+def games_show(games_id):
     for game in games:
-        if game['id'] == game_id:
+        if game['id'] == games_id:
             return game
     return{ "error": "Not Found" }, 404
 
