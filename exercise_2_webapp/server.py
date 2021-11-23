744194c85e4a085ab88fb78cd25b37971ceaaf49
Jonathan <jonathanrios77@yahoo.com>
2021-11-22 17:01:41 -0700

    old file removed

--- exercise_2_webapp/server.py
+++ exercise_2_webapp/server.py
@@ -1,13 +1,23 @@
-from flask import Flask, jsonify, request, render_template
+import csv
+from flask import Flask, request, render_template
+
+DATA_FILE = 'data.csv'
+FIELDNAMES = ['id', 'name', 'year', 'summary']
 
 app = Flask(__name__)
 
-games = [
-{"id": '1', 'name': 'Halo', 'year': 2001},
-{"id": '2', 'name': 'Mass Effect', 'year': 2007},
-{"id": '3', 'name': 'Gears of War', 'year': 2006}
-]
+games = []
 
+def load_data_file():
+  with open(DATA_FILE) as data_file:
+    reader = csv.DictReader(data_file)
+    for row in reader:
+      games.append(row)
+
+def append_data_file(new_row):
+  with open(DATA_FILE, 'a', newline='') as data_file:
+    writer = csv.DictWriter(data_file, FIELDNAMES)
+    writer.writerow(new_row)
 
 @app.route('/games')
 def games_index():
@@ -17,7 +27,8 @@ def games_index():
 def games_show(games_id):
     for game in games:
         if game['id'] == games_id:
-            return game
+            return render_template('index.html', games=games)
+   
     return{ "error": "Not Found" }, 404
 
 @app.route('/movies', methods=['POST'])
@@ -25,6 +36,7 @@ def games_create():
     new_game = request.get_json()
     new_game['id'] = str(len(games) + 1 )
     games.append(new_game)
+    append_data_file(new_game)
     return{ 'message': 'Game created successfully' }, 201
 
 @app.route('/games/<game_id>', methods=['PATCH'])
@@ -51,5 +63,5 @@ def games_delete(game_id):
 
     return { 'error': 'Not Found' }, 404
 
-
+load_data_file()
 app.run()
